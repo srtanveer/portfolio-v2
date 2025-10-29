d7fb7441c306ae091240e20292583066f5b81226
idontbyte <tanveer.cse69@gmail.com>
2025-10-30 01:48:59 +0600

    cv button updated

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
-
 function DownloadCV() {
-  const [isDownloading, setIsDownloading] = useState(false);
-
   const handleDownload = () => {
-    setIsDownloading(true);
-    
-    // Create a link element and trigger download
     const link = document.createElement('a');
     link.href = '/CV_MD_Showaib_Rahman_Tanveer.pdf';
     link.download = 'CV_MD_Showaib_Rahman_Tanveer.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    
-    // Reset button state after a short delay
-    setTimeout(() => {
-      setIsDownloading(false);
-    }, 1000);
   };
 
   return (
@@ -25,10 +13,9 @@ function DownloadCV() {
       <button 
         className="download-cv-btn"
         onClick={handleDownload}
-        disabled={isDownloading}
       >
-        <ion-icon name={isDownloading ? "checkmark-circle-outline" : "download-outline"}></ion-icon>
-        <span>{isDownloading ? "Downloaded!" : "Download CV (PDF)"}</span>
+        <ion-icon name="download-outline"></ion-icon>
+        <span>Download CV (PDF)</span>
       </button>
     </div>
   );
